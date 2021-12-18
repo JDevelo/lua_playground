8117eb87d15c55898eb73e3521a5538b599a447c
Jdevdisc <69683068+JDevelo@users.noreply.github.com>
2021-12-18 18:38:47 -0500

    Initial commit

---
+++ .lua
@@ -0,0 +1,8 @@
+--[[ Lua --]] 
+local version, name, project;
+
+name = "lua_playground";
+version = nil;
+project = nil;
+
+print(name)
---
+++ variables.lua
@@ -0,0 +1,11 @@
+local v, b, n, a, colderry = 2, 4, 5, 7, 69;
+
+
+print(v,b,n)
+
+local x;
+
+x = 100000000000000
+
+
+print(x)
